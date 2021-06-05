9033a66752a873e806f8029b870af3bac790e834
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 09:36:01 -0300

    Update plat_recon.py

--- Code/Python/plat_recon.py
+++ Code/Python/plat_recon.py
@@ -1,5 +1,6 @@
 import platform
 import socket
+import nmap
 
 # Platform
 op_sys = platform.system()
@@ -8,4 +9,20 @@ user_name = platform.os.getlogin()
 network_name = platform.node()
 
 # Network
+def scan_local_hosts():
+	port_scanner = nmap.PortScanner()
+	live_hosts = {'cabe':[], 'wifi':[]}
+	# Cabe:
+	host_info = port_scanner.scan("192.168.1.0/24", arguments="-p 22 --open")
+	for host in host_info:
+		if port_scanner[host].state() == "up":
+			live_hosts['cabe'].append(host)
+	# Wifi:
+	host_info = port_scanner.scan("10.0.0.0/200", arguments="-p 22 --open")
+	for host in host_info:
+		if port_scanner[host].state() == "up":
+			live_hosts['wifi'].append(host)
+	# Return
+	return live_hosts
+
 internal_ip = socket.gethostbyname(socket.gethostname())
