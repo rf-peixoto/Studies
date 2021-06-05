594a1e8b78564f526004a6a6412276b7b833d7f6
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 09:42:48 -0300

    Update plat_recon.py

--- Code/Python/plat_recon.py
+++ Code/Python/plat_recon.py
@@ -10,19 +10,27 @@ network_name = platform.node()
 
 # Network
 def scan_local_hosts():
-	port_scanner = nmap.PortScanner()
-	live_hosts = {'cabe':[], 'wifi':[]}
-	# Cabe:
-	host_info = port_scanner.scan("192.168.1.0/24", arguments="-p 22 --open")
-	for host in host_info:
-		if port_scanner[host].state() == "up":
-			live_hosts['cabe'].append(host)
-	# Wifi:
-	host_info = port_scanner.scan("10.0.0.0/200", arguments="-p 22 --open")
-	for host in host_info:
-		if port_scanner[host].state() == "up":
-			live_hosts['wifi'].append(host)
-	# Return
-	return live_hosts
+    port_scanner = nmap.PortScanner()
+    live_hosts = {'cabe':[], 'wifi':[]}
+    # Cabe:
+    host_info = port_scanner.scan("192.168.1.0/24", arguments="-p 22 --open")
+    # Debug:
+    print(host_info)
+    for host in host_info:
+        if port_scanner[host].state() == "up":
+            live_hosts['cabe'].append(host)
+    #Debug:
+    for i in live_hosts['cabe']:
+            print(i)
+    # Wifi:
+    host_info = port_scanner.scan("10.0.0.0/200", arguments="-p 22 --open")
+    for host in host_info:
+        if port_scanner[host].state() == "up":
+            live_hosts['wifi'].append(host)
+    #Debug:
+    for i in live_hosts['wifi']:
+        print(i)
+    # Return
+    return live_hosts
 
 internal_ip = socket.gethostbyname(socket.gethostname())
