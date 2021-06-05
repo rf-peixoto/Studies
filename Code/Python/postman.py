b63f8294ea66f1f2cce0bf1ba38cae4494e4052d
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 18:30:11 -0300

    Update postman.py

--- Code/Python/postman.py
+++ Code/Python/postman.py
@@ -2,10 +2,8 @@
 # PyPostman v0.0.3
 # Simple tool to manage HTTP Requests
 # Usage:
-# python postman.py [DOMAIN]
+# python postman.py [DOMAIN] [PATH]
 # ====================================================== #
-
-
 import sys
 import socket
 import urllib.parse
@@ -13,31 +11,32 @@ import urllib.parse
 # CONFIG:
 method = 'GET' # GET / PUT / POST / OPTIONS / DELETE / MOVE
 host = sys.argv[1]
+path = sys.argv[2]
 port = 80
-user_agent = 'AlexaBot'
+user_agent = 'BotName'
 referer = 'no-referer-when-downgrade'
 cookie = ''
 payload = urllib.parse.quote('')
 content_length = len(payload)
-response_size = 8192
+bytes_to_receive = 1024
 
 # REQUEST:
 # Change '/' to path:
-request = """{0} / HTTP/1.0\r\n
-Host: {1}\r\n
-User-Agent: {2}\r\n
+request = """{0} {1} HTTP/1.0\r\n
+Host: {2}\r\n
+User-Agent: {3}\r\n
 Accept: text/html, application/xhtml+xml, application/xml;q=0.9,*/*;q=0.8\r\n
 Accept-Language: en-US, en;q=0.5\r\n
 Accept-Encoding: gzip, deflate\r\n
 Cache-Control: no-cache\r\n
 Pragma: no-cache\r\n
-Referer: {3}\r\n
-Content-Length: {4}\r\n
+Referer: {4}\r\n
+Content-Length: {5}\r\n
 DNT: 1\r\n
 Connection: close\r\n
-Cookie = {5}\r\n
+Cookie = {6}\r\n
 \r\n
-{6}""".format(method, host, user_agent, referer, content_length, cookie, payload)
+{7}""".format(method, path, host, user_agent, referer, content_length, cookie, payload)
 
 # SOCKET
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,4 +44,4 @@ s.connect((host, port))
 
 # SEND
 s.send(request.encode())
-print(s.recv(response_size).decode())
+print(s.recv(bytes_to_receive).decode())
