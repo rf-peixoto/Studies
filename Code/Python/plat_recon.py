a760108b352e3db981273feefdce7ac9c851c21c
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-05 08:55:30 -0300

    Update plat_recon.py

--- Code/Python/plat_recon.py
+++ Code/Python/plat_recon.py
@@ -1,6 +1,11 @@
 import platform
+import socket
 
+# Platform
 op_sys = platform.system()
 arch = platform.architecture()
 user_name = platform.os.getlogin()
 network_name = platform.node()
+
+# Network
+internal_ip = socket.gethostbyname(socket.gethostname())
