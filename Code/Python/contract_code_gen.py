8a27607a4854c1d4ef5b0b20f644c425876c7a95
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-14 21:36:13 -0300

    Create contract_code_gen.py

---
+++ Code/Python/contract_code_gen.py
@@ -0,0 +1,45 @@
+import os
+import qrcode
+import secrets
+from random import randint
+
+# Get data:
+
+date = input("Data: 00-00-00 ")
+client = input("Email do Destinatário: ").lower()
+coin = input("Moeda: ").upper()
+amount = input("Quantidade/valor: ")
+
+# Generate unique code:
+
+f_label = secrets.token_urlsafe(8)
+s_label =  str(randint(0, 999)).zfill(4)
+t_label = secrets.token_urlsafe(8)
+
+unique_code = f_label + "-" + s_label + "-" + t_label
+
+compile_string = """Data: {0}
+Remetente: EMAIL
+Destinatário: {1}
+Moeda: {2}
+Valor: {3}
+Identificador: {4}""".format(date, client, coin, amount, unique_code)
+
+# Create QR Code:
+qr = qrcode.QRCode(version=None, # Int: 1 to 40 that controls the size of the img. Set to None and use the fit parameter when making the code to determine this automatically.
+                   error_correction=qrcode.constants.ERROR_CORRECT_L,
+                   box_size=10,
+                   border=4)
+# Generate
+qr.add_data(compile_string)
+qr.make(fit=True)
+# Colors and Image:
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Saving:
+os.system("touch Recibo_{0}_{1}.png".format(date, unique_code))
+with open("Recibo_{0}_{1}.png".format(date, unique_code), "wb") as image:
+    img.save(image)
+
+# Ending.
+print("Done.")
