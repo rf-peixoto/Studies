a6d0ae7c6fcf34f457f661abce5f902e26d82cfa
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-06-14 21:17:24 -0300

    Update qr_codes.py

--- Code/Python/qr_codes.py
+++ Code/Python/qr_codes.py
@@ -2,7 +2,7 @@
 import qrcode
 
 # Create Object
-qr = qrcode.QRCode(version=1,
+qr = qrcode.QRCode(version=None, # Int: 1 to 40 that controls the size of the img. Set to None and use the fit parameter when making the code to determine this automatically.
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=10,
                    border=4)
