04184952f21d4848fce5e301b0a74c82e3ba3ce0
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-07-08 18:12:56 -0300

    Undo last commit on bkp_util.py

--- Code/Python/bkp_util.py
+++ Code/Python/bkp_util.py
@@ -1,26 +1,21 @@
-# ================================================== #
-# Backup Util
-# ================================================== #
-import sys
+# WIP
+# ===================================== #
+# Backup Folder
+# ===================================== #
 import zipfile
-from datetime import datetime
-# -------------------------------------------------- #
-if len(sys.argv) <= 1:
-    print("Usage: {0} [folder_name]".format(sys.argv[0]))
-    sys.exit()
-# -------------------------------------------------- #
-folder_name = str(sys.argv[1])
-
-# Get Date:
-time = datetime.now()
-time_string = "{0}{1}{2}".format(str(time.month).zfill(2), str(time.day).zfill(2), str(time.year).zfill(4))
-# -------------------------------------------------- #
-# Save file:
-print("Compressing.")
-
-backup = zipfile.ZipFile(file="{0}_{1}.bkp".format(folder_name, time_string), mode="w", compresslevel=9)
-backup.write(folder_name, compress_type=zipfile.ZIP_DEFLATED)
+import os
+# Input folder:
+folder_name = input("Folder name: ")
+directory = os.path.abspath(folder_name)
+zip_name = "{0}.bkp.zip".format(directory)
+# Create File:
+backup = zipfile.ZipFile(file=zip_name, mode="w", compresslevel=9)
+for foldername, subfolder, files in os.walk(directory):
+    backup.write(foldername, compress_type=zipfile.ZIP_DEFLATED)
+for f in files:
+    tmp_name = os.path.basename(folder_name) + '_'
+    if f.startswith(tmp_name) and f.endswith('.zip'):
+        continue
+    backup.write(os.path.join(foldername, f), compress_type=zipfile.ZIP_DEFLATED)
 backup.close()
-
-# -------------------------------------------------- #
-print("Done.")
+#backup.write(folder_name, compress_type = zipfile.ZIP_DEFLATED)
