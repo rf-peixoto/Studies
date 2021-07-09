3e0aec8a15592bdf762c2a5c572b299bc73d1738
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-07-09 15:35:06 -0300

    Update gpg_file_brute.sh

--- Code/Bash/gpg_file_brute.sh
+++ Code/Bash/gpg_file_brute.sh
@@ -1,5 +1,5 @@
 for i in $(cat $1):
 do
   echo "Testing password: $i"
-  echo "$i" | gpg --passphrase-fd 0 -q --batch --no-tty --output result.txt -->
+  echo "$i" | gpg --passphrase-fd 0 -q --batch --no-tty --output result.txt --decrypt file.gpg 2> /dev/null
 done
