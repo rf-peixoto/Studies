f17e526aed68676e9d6aacd0a4152a70ed7f5728
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-01 10:33:52 -0300

    Update linux_utils.sh

--- Code/Bash/linux_utils.sh
+++ Code/Bash/linux_utils.sh
@@ -2,3 +2,4 @@ sudo iptraf-ng                        # Network monitor
 systemctl list-units --type=service   # List all services
 sudo chattr +i [file]                 # Make a file immutable. Use -i to revert. Use +a to 'append only', for logs.
 lsattr [file]                         # List the attributes of a file. Check man chattr to understand how they work.
+ldd [file]                            # Show C libs udes in a ELF file.
