59e84d4fd90bd7aa6abc66ff7e6d584c13644207
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-04 09:23:22 -0300

    Update banner-grabbing.py

--- Code/Python/banner-grabbing.py
+++ Code/Python/banner-grabbing.py
@@ -1,12 +1,10 @@
-# Banner Grabbing
-# #!/usr/bin/python
+#!/usr/bin/python
 import socket
-
+# Target
 host = input("Host: ")
 port = int(input("Port: "))
-
+# Socket
 sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection = sckt.connect((host, port))
-
-banner = sckt.recv(1024)
-print(banner)
+# Banner
+print(sckt.recv(1024))
