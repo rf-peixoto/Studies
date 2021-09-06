f3471fd651b21f07c9745a6b462b0598c8141710
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-06 16:07:14 -0300

    Create monitor.sh

---
+++ Code/WiFi/monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# $1 : interface
+
+for i in {1..11}
+do
+  echo "\n[+] Ouvindo canal: $i\n"
+  ifconfig $1 down
+  iwconfig $1 channel $i
+  iwconfig $1 mode Monitor
+  ifconfig $1 up
+  sleep 2
+  tcpdump -vv -i $1 -c 10 | grep "Beacon" | awk '{print $12, - CH $24}'
+  echo "\n"
+done
+  
