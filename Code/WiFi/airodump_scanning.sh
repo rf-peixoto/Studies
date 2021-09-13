302566365cb951012fc4d26e1b92fe4ef3aafaf1
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-13 08:23:05 -0300

    Update airodump_scanning.sh

--- Code/WiFi/airodump_scanning.sh
+++ Code/WiFi/airodump_scanning.sh
@@ -1,2 +1,3 @@
 # Escaneando redes com airodump-ng:
+# Argumento opcional: --write-interval [N] (Atualizar o arquivo a cada N segundos)
 airodump-ng [interface] --essid-regex [SSID] --output-form netxml -w [dump file]
