0cd89c7df8319fe9b91d9f1810ca4e16d91e4745
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-26 14:48:21 -0300

    Create colors.sh

---
+++ Code/Bash/colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# For reference: https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+# Colors:
+echo -e "\e[1;31m[z]\e[0m" # Red
+echo -e "\e[32m[+]\e[0m" # Green
+echo -e "\e[33m[!]\e[0m" # Yellow
+
+# Effects:
+echo -e "Normal \e[5mBlink"
