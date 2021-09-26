1ef1f451b095a964b411a0191bc1e8ba2c6521c0
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-09-26 14:52:34 -0300

    Update colors.sh

--- Code/Bash/colors.sh
+++ Code/Bash/colors.sh
@@ -6,6 +6,13 @@
 echo -e "\e[1;31m[z]\e[0m" # Red
 echo -e "\e[32m[+]\e[0m" # Green
 echo -e "\e[33m[!]\e[0m" # Yellow
+echo -e "\e[34m[!]\e[0m" # Blue
+echo -e "\e[35m[!]\e[0m" # Magenta
+echo -e "\e[36m[!]\e[0m" # Cyan
+echo -e "\e[97m[!]\e[0m" # White
+
+# Background Colors:
+echo -e "Default \e[41m[X]\e[om"
 
 # Effects:
-echo -e "Normal \e[5mBlink"
+echo -e "Normal \e[5mBlink\e[0m"
