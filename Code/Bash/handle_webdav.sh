483dab258685d86aba85563c637bc452f0276b1c
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2021-10-26 20:08:58 -0300

    Update handle_webdav.sh

--- Code/Bash/handle_webdav.sh
+++ Code/Bash/handle_webdav.sh
@@ -1,1 +1,4 @@
 cadaver https://127.0.0.1/webdav/
+davtest -url https://127.0.0.1/webdav/
+curl -v -X PUT -d "Your DATA" https://127.0.0.1/webdav/new_file.ext
+curl -v hhtps://127.0.0.1/webdav/ --upload-file /path/file
