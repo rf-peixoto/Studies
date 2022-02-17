2d78192fceef833197d4514c732bd423c184c0fd
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-02-17 16:20:13 -0300

    Update bash_tricks.sh

--- Code/Bash/bash_tricks.sh
+++ Code/Bash/bash_tricks.sh
@@ -36,3 +36,6 @@ du -h -max-depth=2
 
 # Check files and folders size:
 du -sh *
+
+# Check for duplicates with md5sum and file size:
+find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
