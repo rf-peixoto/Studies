bcf118a7da244c5c541a94c2bf14f86e5150c63e
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-02-23 13:56:19 -0300

    Update cpp_modules_and_libraries.sh

--- Code/C/cpp_modules_and_libraries.sh
+++ Code/C/cpp_modules_and_libraries.sh
@@ -2,7 +2,7 @@
 c++ -std=c++17 -c module.cpp
 
 # Turn modules into libraries:
-ar rcs libtools.a module.o # <other_modules>
+ar rcs libmodule.a module.o # <other_modules>
 
 # Compile main application:
 c++ -std=c++17 -c main.cpp
