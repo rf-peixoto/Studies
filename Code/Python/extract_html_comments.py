9fcabbe27cda88896aa397c35163a3f30929bd22
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-05-27 10:07:08 -0300

    Create extract_html_comments.py

---
+++ Code/Python/extract_html_comments.py
@@ -0,0 +1,30 @@
+import sys, re
+import requests
+
+# Request Page:
+print("\033[92m[*]\033[00m Requesting data...")
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+    }
+
+page = requests.get(sys.argv[1], headers=headers)
+
+# Verify Request:
+if page.status_code != 200:
+    print("\033[31m[-] Error while getting page:")
+    print("\033[31mStatus Code: {0} Reason: {1}\033[00m".format(page.status_code, page.reason))
+else:
+    # Parsing Response:
+    body = page.content.decode()
+    print("\033[92m[+]\033[00m Body received. Size: {0}".format(len(body)))
+    # Searching comments:
+    print("\033[92m[+]\033[00m Parsing comments...\n")
+    comments = re.findall('<!--(.*?)-->', body)
+    if len(comments) > 0:
+        for comm in comments:
+            print("\033[94m[*]\033[0m {0}".format(comm))
+    else:
+        print("\033[31m[-] Something went wrong. No comment was found.")
+
+# Finish program:
+print("\n\033[92m[*]\033[00m Done.")
