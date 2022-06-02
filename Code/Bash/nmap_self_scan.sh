584ba7e2754f86fd25f054ee833ae10d59bfac40
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-02 15:25:27 -0300

    Update nmap_self_scan.sh

--- Code/Bash/nmap_self_scan.sh
+++ Code/Bash/nmap_self_scan.sh
@@ -1,1 +1,18 @@
 clear && sudo nmap -sV -Pn --script=vuln --open -D RND:16 $(curl ifconfig.me) #-oX output.xml
+
+# Categories to use on --script=CATEGORY
+
+#    auth
+#    broadcast
+#    brute
+#    default
+#    discovery
+#    dos
+#    exploit
+#    external
+#    fuzzer
+#    intrusive
+#    malware
+#    safe
+#    version
+#    vuln
