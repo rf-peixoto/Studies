13e78eb4e8d54e9e9324ae467460917eb78c8960
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-11 10:56:24 -0300

    Update compile_python_script.sh

--- Code/Bash/compile_python_script.sh
+++ Code/Bash/compile_python_script.sh
@@ -1,2 +1,2 @@
 #pyinstaller --onefile --name output --key $1 --noconsole $2
-nuitka3 --standalone --assume-yes-for-downloads $1
+nuitka3 --verbose --standalone --assume-yes-for-downloads --follow-stdlib --follow-imports $1
