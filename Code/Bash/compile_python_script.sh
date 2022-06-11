0003cee722379acc9df718884acd9488eb09ee45
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-11 12:07:13 -0300

    Update compile_python_script.sh

--- Code/Bash/compile_python_script.sh
+++ Code/Bash/compile_python_script.sh
@@ -2,3 +2,5 @@
 # Use --standalone or --onefile
 # --onefile requires --linux-onefile-icon=/usr/share/icons/hicolor/scalable/apps/org.gnome.Shell.Extensions.svg
 nuitka3 --remove-output --standalone --assume-yes-for-downloads --follow-stdlib --follow-imports $1
+# Options:
+# --enable-plugin=tk-inter
