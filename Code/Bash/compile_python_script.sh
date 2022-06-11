05af0e9b73681c5c80284d6f0cbbbc27052b9920
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-11 12:19:05 -0300

    Update compile_python_script.sh

--- Code/Bash/compile_python_script.sh
+++ Code/Bash/compile_python_script.sh
@@ -1,6 +1,8 @@
 #pyinstaller --onefile --name output --key $1 --noconsole $2
+
+# pip install nuitka nuitka3
 # Use --standalone or --onefile
 # --onefile requires --linux-onefile-icon=/usr/share/icons/hicolor/scalable/apps/org.gnome.Shell.Extensions.svg
-nuitka3 --remove-output --standalone --assume-yes-for-downloads --follow-stdlib --follow-imports $1
+python -m nuitka --remove-output --standalone --assume-yes-for-downloads --follow-stdlib --follow-imports $1
 # Options:
 # --enable-plugin=tk-inter
