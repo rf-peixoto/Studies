bd98b316d5f79deb5448bcd5ba09e46e51dd82a0
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-11 11:40:02 -0300

    Update compile_python_script.sh

--- Code/Bash/compile_python_script.sh
+++ Code/Bash/compile_python_script.sh
@@ -1,3 +1,4 @@
 #pyinstaller --onefile --name output --key $1 --noconsole $2
 # Use --standalone or --onefile
+# --onefile requires --linux-onefile-icon=/usr/share/icons/hicolor/scalable/apps/org.gnome.Shell.Extensions.svg
 nuitka3 --remove-output --standalone --assume-yes-for-downloads --follow-stdlib --follow-imports $1
