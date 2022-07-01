af53b7d0c9220182897e0add512dd78076d0a081
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-07-01 10:58:32 -0300

    Update cat_api.py

--- Code/Python/cat_api.py
+++ Code/Python/cat_api.py
@@ -1,10 +1,11 @@
 import requests
 import webbrowser
-import json
-
-# Request new picture:
-link = requests.get("https://api.thecatapi.com/v1/images/search").content.decode()>
-img = json.loads(link)
-
-# Show:
-webbrowser.open(img['url'], new=2)
+import json  
+from time import sleep
+#-----#
+while True:
+    link = requests.get("https://api.thecatapi.com/v1/images/search").content.decode()[1:-1]
+    img = json.loads(link)
+    webbrowser.open(img['url'], new=2)
+    sleep(3600)
+#-----#
