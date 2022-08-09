644a7af214b54a61d70947d8e59ff6316886f30a
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-08-09 08:38:06 -0300

    Update general_tips.sh

--- Code/Bash/general_tips.sh
+++ Code/Bash/general_tips.sh
@@ -1,3 +1,34 @@
+# Enviar arquivo criptografado:
+cat $1 | openssl aes-256-cbc -a -e -pass pass:password | netcat -l -p 8080
+
+# Criar popup em máquina Windows via SMB:
+echo "TEXTO" | smbclient -M HOST_WINDOWS
+
+# Criar popup no Gnome:
+notify-send ["<Título>"] "Texto"
+
+# Enviar arquivo via icmp com hping:
+hping3 $IP --icmp --sign MSGID1 -d 50 -c 1 --file $1
+
+# Monitorar consultas feitas ao MySQL:
+watch -n 1 mysqladmin --user=USER --password=PASSWORD processlist
+
+# Listar programas conectados à internet no momento:
+lsof -P -i -n | cut -f 1 -d " "| uniq | tail -n +2
+
+# Listar arquivos modificados nos últimos 30 minutos:
+# (útil para threat hunting)
+sudo find / -mmin 30 -type f
+
+# Encontrar arquivos criados durante determinado período AAAA/MM/DD:
+find . -type f -newermt "2022-08-05" ! -newermt "2022-08-09"
+
+# Monitorar o uso de memória.
+watch vmstat -sSM
+
+# Enviar comando para vários hosts conectados:
+<COMANDO> | tee >(ssh hosta) >(ssh hostb) >(ssh hostc)
+
 # Alternativa ao man para  buscar informações sobre binário:
 wtf $1 # Ex: wtf ssh -> SSH: secure shell
 
