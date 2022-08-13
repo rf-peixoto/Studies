f5fd88b773e618ed35f1d02d338a297963014482
rf-peixoto <rodrigo-f-p@hotmail.com>
2022-08-13 19:44:05 -0300

    Add stuff.

--- Code/Python/snippets.py
+++ Code/Python/snippets.py
@@ -1,5 +1,11 @@
-# Read sys input:
+# Ler valores no sys.args
 sys.stdin.read()
 
-# Print:
-sys.stdout.write("stuff")
+# O mesmo que print()
+sys.stdout.write("Hello World")
+
+# Operações de multiplicação são menos custosas
+# para o processador que operações de divisão.
+# A operação abaixo é mais eficiente que x = 10 / 2
+x = 10 * 0.5
+
