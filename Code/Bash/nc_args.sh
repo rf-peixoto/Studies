3029a46a1b05ca97ba2104991ee97e41839e75ea
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-08-22 18:22:40 -0300

    Create nc_args.sh

---
+++ Code/Bash/nc_args.sh
@@ -0,0 +1,7 @@
+# --send-only to ignore received data. Use as protection
+# on reverse connections. Imagine that you have nc ready
+# to send code to be executed on a host. Use this.
+nc --send-only
+
+# ms: milliseconds;
+--delay 500ms 30s 5m 1h
