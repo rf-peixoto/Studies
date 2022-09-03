4d6cff2a3b099034474ce889926015beb5e1cd3e
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-09-03 18:51:08 -0300

    Update bin_stuff.py

--- Code/Python/bin_stuff.py
+++ Code/Python/bin_stuff.py
@@ -7,3 +7,10 @@ for i in sys.argv[1]:
 # For use, remove '
 for i in tmp:
     print(chr(i))
+
+# Example:
+string = [0b1001000, 0b1100101, 0b1101100, 0b1101100, 0b1101111, 0b101100, 0b100000, 0b1110111, 0b1101111, 0b1110010, 0b1101100, 0b1100100]
+decoded = ""
+for i in string:
+    decoded += chr(i)
+print(decoded)
