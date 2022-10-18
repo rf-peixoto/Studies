10bea074147a8ce6533a1072ba848056a7cda6d2
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-10-18 08:11:13 -0300

    Create vulnerable_file.c

---
+++ Code/C/vulnerable_file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    char input[12];
+    printf("Please enter your password: ");
+    scanf("%s", input);
+    printf("Your password is %s", input);
+    return(0);
+}
