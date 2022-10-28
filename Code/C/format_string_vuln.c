2c9a81590a5276a3e167e329a7ed00de90fe9fc7
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-10-28 07:23:31 -0300

    Update format_string_vuln.c

--- Code/C/format_string_vuln.c
+++ Code/C/format_string_vuln.c
@@ -4,8 +4,19 @@
 // to get the actual memory address.
 
 int main () {
-  char buffer [8];
-  gets(buffer);
-  printf(buffer);
-  printf("\n");
+
+  // Check stack with %p
+    char buffer [8];
+    gets(buffer);
+    printf(buffer);
+    printf("\n");
+  
+  // Check with %s or use %n to write an address
+    printf("%s%s%s%s%s%s%s%s%s%s%s%s");
+    printf("\n");
+  
+  // View part of the stack as eight-digit padded-hexadecimal
+    printf("%08x.%08x.%08x.%08x.%08x\n");
+    printf("\n");
+
 }
