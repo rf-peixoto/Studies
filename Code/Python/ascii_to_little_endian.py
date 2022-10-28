a3acda77930d0b96efb8787784a2430a3d1d1819
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-10-28 08:01:58 -0300

    Update ascii_to_little_endian.py

--- Code/Python/ascii_to_little_endian.py
+++ Code/Python/ascii_to_little_endian.py
@@ -3,5 +3,8 @@ import sys
 data = sys.argv[1]
 tmp = ""
 for c in data[::-1]:
-    tmp += "\\x" + str(c.encode("utf-8").hex())
+    if sys.argv[2] == "x":
+        tmp += "\\x" + str(c.encode("utf-8").hex())
+    else:
+        tmp += str(c.encode("utf-8").hex())
 print(tmp)
