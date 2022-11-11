809513da2e8805d6845bdc31d06aaeccc8e7fc8e
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-11-11 08:57:47 -0300

    Update async_runtime.rs

--- Code/Rust/async_runtime.rs
+++ Code/Rust/async_runtime.rs
@@ -1,7 +1,12 @@
 // Ref: https://tokio.rs/
+
 // On Cargo.toml:
 tokio = { version = "1", features = ["full"] }
+
 // Spawn:
 tokio::spawn(async move {
     // Do your thing;
 });
+
+// Sleep:
+tokio::time::sleep(Duration::from_millis(sleep_ms)).await;
