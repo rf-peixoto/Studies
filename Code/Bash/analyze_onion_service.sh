a45206058888035348fe1621d1dd48639f3d7062
Corvo <rodrigo-f-p@hotmail.com>
2023-01-19 07:13:13 -0300

    Create analyze_onion_service.sh

---
+++ Code/Bash/analyze_onion_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start tor:
+service tor start
+
+# Prepare socat:
+# Onion URL without protocol. Ex: bashdbajshbdasjhdbaifgbsdiyfsdg.onion:80
+socat TCP4-LISTEN:8000,reuseaddr,fork SOCKS4A:127.0.0.1:ONIONURL:PORT,socksport=9050
+
+# Use with localhost:80. Ex:
+curl 127.0.0.1:80
