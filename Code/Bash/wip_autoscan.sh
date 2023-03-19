17f688d389531a1153607061e417a1d580df332c
Corvo <rodrigo-f-p@hotmail.com>
2023-03-19 08:35:37 -0300

    Create

---
+++ Code/Bash/wip_autoscan.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Work in progress
+
+clear
+
+# SETUP:
+CLEAR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;32m'
+
+# Start:
+echo -e "[${BLUE}*${CLEAR}] Running on ${GREEN}$1${CLEAR}"
+
+# Subdomains:
+assetfinder $1 | sort -u > subdomains.txt
+echo -e "    Found ${BLUE}$(wc -l subdomains.txt)${CLEAR} targets."
+
+# Nuclei:
+echo -e "[${BLUE}*${CLEAR}] Now scanning..."
+proxychains nuclei -silent -l subdomains.txt -o nuclei.txt
+
+# Print results:
+echo -e "[${BLUE}*${CLEAR}] Results:"
+echo -e "[${BLUE}low${CLEAR}]"
+grep -r '\[low\]' nuclei.txt
+echo -e "[${YELLOW}medium${CLEAR}]"
+grep -r '\[medium\]' nuclei.txt
+echo -e "[${RED}high${CLEAR}]"
+grep -r '\[high\]' nuclei.txt
+echo -e "[${RED}critical${CLEAR}]"
+grep -r '\[critical\]' nuclei.txt
+echo -e "[${GREEN}unknow${CLEAR}]"
+grep -r '\[unknow\]' nuclei.txt
+echo -e "[${GREEN}cve${CLEAR}]"
+grep -r '\[cve\]' nuclei.txt
+
+
+
+# Remove tmp files:
+rm *.txt
