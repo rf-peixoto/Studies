3362a5272d7f8de126cc49c912e19f8aa6dcb6ad
Corvo <rodrigo-f-p@hotmail.com>
2023-03-20 08:48:34 -0300

    Create

---
+++ Code/Python/get_lib_info.py
@@ -0,0 +1,13 @@
+import secrets
+
+dir(secrets)
+dir(secrets.token_bytes)
+
+#import sys
+
+#libname = sys.argv[1]
+#try:
+#    exec('import {0}'.format(libname))
+#    exec('dir({0})'.format(libname))
+#except Exception as error:
+#    print(error)
