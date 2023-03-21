05ba3dcb13f60a5eda2aefdbff5e23431f4cfed9
Corvo <rodrigo-f-p@hotmail.com>
2023-03-21 13:45:02 -0300

    Update wip_autoscan.sh

--- Code/Bash/wip_autoscan.sh
+++ Code/Bash/wip_autoscan.sh
@@ -10,7 +10,6 @@ usage() {
 EOF
 }
 
-
 # Check args:
 if [[ -z $1 || $1 = @(-h|--help) ]]; then
   usage
@@ -34,22 +33,28 @@ echo -e "    Found ${BLUE}$(wc -l subdomains.txt)${CLEAR} targets."
 
 # Nuclei:
 echo -e "[${BLUE}*${CLEAR}] Now scanning. This can take a while."
-proxychains -q nuclei -env-vars -scan-all-ips -silent -l subdomains.txt -o nuclei.txt
+proxychains -q nuclei -env-vars -silent -l subdomains.txt -o nuclei.txt
+proxychains -q sudo nmap --spoof-mac -sV -Pn --reason -f --data-length 16 --script=vuln -D RND:16 -iL subdomains.txt -oG nmap.txt
+
+# ZAP main URL:
+# curl "http://localhost:8080/JSON/ascan/action/scan/?apikey=APIKEY&url=$1&recurse=true&inScopeOnly=&scanPolicyName=&method=&postData=&contextId="
+# curl "http://localhost:8080/JSON/core/view/alerts/?apikey=APIKEY&baseurl=$1&start=0&count=10"
+
 
 # Print results:
 echo -e "[${BLUE}*${CLEAR}] Results:"
 echo -e "[${BLUE}low${CLEAR}]"
-grep -r '\[low\]' nuclei.txt
+grep --color='auto' -r '\[low\]' nuclei.txt
 echo -e "[${YELLOW}medium${CLEAR}]"
-grep -r '\[medium\]' nuclei.txt
+grep --color='auto' -r '\[medium\]' nuclei.txt
 echo -e "[${RED}high${CLEAR}]"
-grep -r '\[high\]' nuclei.txt
+grep --color='auto' -r '\[high\]' nuclei.txt
 echo -e "[${RED}critical${CLEAR}]"
-grep -r '\[critical\]' nuclei.txt
+grep --color='auto' -r '\[critical\]' nuclei.txt
 echo -e "[${GREEN}unknow${CLEAR}]"
-grep -r '\[unknow\]' nuclei.txt
+grep --color='auto' -r '\[unknow\]' nuclei.txt
 echo -e "[${GREEN}cve${CLEAR}]"
-grep -r '\[cve\]' nuclei.txt
+grep --color='auto' -r '\[cve\]' nuclei.txt
 
 # Remove tmp files:
-rm *.txt
+#rm *.txt
