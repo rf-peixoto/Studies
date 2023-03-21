f22d148eb86a70805f36417cf8d1ebe668fe9840
Corvo <rodrigo-f-p@hotmail.com>
2023-03-21 08:24:28 -0300

    Update grep_nuclei_findings.sh

--- Code/Bash/grep_nuclei_findings.sh
+++ Code/Bash/grep_nuclei_findings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Grep most interesting findings from nuclei raw output:
-grep -r '\[low\]\|\[medium\]\|\[high\]\|\[critical\]\|\[cve\]' $1
+grep --color='auto' -r '\[low\]\|\[medium\]\|\[high\]\|\[critical\]\|\[cve\]' $1
