af1e5d34eb7ae402928e215c4897d161eebb5a91
Corvo <rodrigo-f-p@hotmail.com>
2023-04-05 19:15:35 -0300

    Update code_caves.sh

--- Code/Bash/code_caves.sh
+++ Code/Bash/code_caves.sh
@@ -1,3 +1,3 @@
 # Code caves are sections on binary with space to execute a payload. You can find then with:
-./Cminer [file] [Minimum space] # Ex: ./Cminer Game.exe 512
+./Cminer [file] [Minimum space] # Ex:  ./Cminer Game.exe 512
 # Do not pass size to find any cave, regardless its size.
