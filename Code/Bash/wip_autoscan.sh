254e8f3b46361e34d0ce2fc120b6b297d850ba79
Corvo <rodrigo-f-p@hotmail.com>
2023-05-26 08:27:21 -0300

    Update wip_autoscan.sh

--- Code/Bash/wip_autoscan.sh
+++ Code/Bash/wip_autoscan.sh
@@ -26,43 +26,49 @@ YELLOW='\033[1;32m'
 
 # Start:
 echo -e "[${BLUE}*${CLEAR}] Running on ${GREEN}$1${CLEAR}"
+mkdir $1
 
 # Subdomains:
-echo $1 > subdomains.txt
-assetfinder $1 | sort -u >> subdomains.txt
-#assetfinder $1 | alterx -silent | sort -u >> subdomains.txt
-
-echo -e "    Found ${BLUE}$(wc -l subdomains.txt)${CLEAR} targets."
+echo $1 > $1/subdomains.txt
+assetfinder $1 | sort -u >> $1/subdomains.txt
+echo -e "    Found ${BLUE}$(wc -l $1/subdomains.txt)${CLEAR} targets."
 
 # Nuclei:
 echo -e "[${BLUE}*${CLEAR}] Now scanning. This can take a while."
-nuclei -follow-host-redirects -passive -env-vars -silent -l subdomains.txt -o nuclei.txt
+nuclei -follow-host-redirects -env-vars -silent -l $1/subdomains.txt -o $1/nuclei.txt
 
-# nmap:
-sudo nmap --spoof-mac=6 -sV -Pn --reason -f --data-length 16 --script=vuln -D RND:16 -iL subdomains.txt -oG nmap.txt 2>/dev/null
+# naabu/nmap:
+echo -e "[${BLUE}*${CLEAR}] Scanning ports."
+naabu -silent -list $1/subdomains.txt -sD -display-cdn -scan-all-ips | sort -u > $1/naabu.txt
+sudo nmap --spoof-mac=6 -sV -Pn --reason -f --data-length 16 --script=vuln,malware -D RND:16 -iL $1/subdomains.txt -oG $1/nmap.txt 2>/dev/null
 
 # sqlmap:
-sqlmap -u 'http://$1/' --random-agent --forms --crawl 10 --batch --skip-waf --dbs --level 5
+sqlmap -u 'http://$1/' --random-agent --forms --crawl 10 --batch --skip-waf --dbs --level 5 --no-logging --output-dir=$1/sqlmap.txt
 
 # ZAP main URL:
-# curl "http://localhost:8080/JSON/ascan/action/scan/?apikey=APIKEY&url=$1&recurse=true&inScopeOnly=&scanPolicyName=&method=&postData=&contextId="
-# curl "http://localhost:8080/JSON/core/view/alerts/?apikey=APIKEY&baseurl=$1&start=0&count=10"
+# curl "http://localhost:8080/JSON/ascan/action/scan/?apikey=API&url=$1&recurse=true&inScopeOnly=&scanPolicyName=&method=&postData=&contextId="
+# curl "http://localhost:8080/JSON/core/view/alerts/?apikey=API&baseurl=$1&start=0&count=10"
+
 
 # Print results:
 echo ""
 echo -e "[${BLUE}*${CLEAR}] Results:"
 echo -e "[${BLUE}low${CLEAR}]"
-grep --color='auto' -r '\[low\]' nuclei.txt
+grep --color='auto' -r '\[low\]' $1/nuclei.txt
 echo -e "[${YELLOW}medium${CLEAR}]"
-grep --color='auto' -r '\[medium\]' nuclei.txt
+grep --color='auto' -r '\[medium\]' $1/nuclei.txt
 echo -e "[${RED}high${CLEAR}]"
-grep --color='auto' -r '\[high\]' nuclei.txt
+grep --color='auto' -r '\[high\]' $1/nuclei.txt
 echo -e "[${RED}critical${CLEAR}]"
-grep --color='auto' -r '\[critical\]' nuclei.txt
+grep --color='auto' -r '\[critical\]' $1/nuclei.txt
 echo -e "[${GREEN}unknow${CLEAR}]"
-grep --color='auto' -r '\[unknow\]' nuclei.txt
+grep --color='auto' -r '\[unknow\]' $1/nuclei.txt
 echo -e "[${GREEN}cve${CLEAR}]"
-grep --color='auto' -r '\[cve\]' nuclei.txt
+grep --color='auto' -r '\[cve\]' $1/nuclei.txt
 
 # Remove tmp files:
-rm *.txt
+rm *.txt 2>/dev/null && rm ~/.config/nuclei/*.cfg 2>/dev/null
+echo ""
+
+# Notify on GNOME:
+notify-send "Finished scan on $1"
