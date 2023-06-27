63987f24082eee9c44d933d21166ca4f71384971
Corvo <rodrigo-f-p@hotmail.com>
2023-06-27 20:39:14 -0300

    Update extract_private_emails.sh

--- Code/Bash/extract_private_emails.sh
+++ Code/Bash/extract_private_emails.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -v '@outlook\|@hotmail\|@gmail\|@yahoo\|@yandex\|@bol\.com\|@terra\.com\|@ig\.com\|@uol\.com\|@live\.\|@aol\.com\|@qq\.com\|@msn\.com\.br' $1
+grep -v '@ibest\.com\.br\|@outlook\|@hotmail\|@gmail\|@yahoo\|@yandex\|@bol\.com\|@terra\.com\|@ig\.com\|@uol\.com\|@live\.\|@aol\.com\|@qq\.com\|@msn\.com\.br' $1
