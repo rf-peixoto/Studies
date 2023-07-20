57b9246c3898a6971ff8105267ff81d51dec1b3c
Corvo <rodrigo-f-p@hotmail.com>
2023-07-20 11:50:02 -0300

    Create auto_smtp.sh

---
+++ Code/Bash/auto_smtp.sh
@@ -0,0 +1,17 @@
+sudo apt-get install postfix
+sudo postconf -e "smtpd_port = 25"
+sudo postconf -e "smtpd_relayhost = localhost"
+sudo vi /etc/mail/virtual/domains
+echo "@$1 mail exchanger = 1 localhost" >> /etc/mail/virtual/domains
+sudo vi /etc/postfix/sieve/main.cf
+echo "smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination, check_policy_service unix:private/policy-spf" >> /etc/postfix/sieve/main.cf
+sudo vi /etc/postfix/dmarc.conf
+echo "dmarc_policy = quarantine" >> /etc/postfix/dmarc.conf
+sudo vi /etc/postfix/dkim.conf
+echo "dkim_selector = default" >> /etc/postfix/dkim.conf
+echo "dkim_domain = $1" >> /etc/postfix/dkim.conf
+echo "dkim_private_key = /etc/postfix/dkim.private" >> /etc/postfix/dkim.conf
+echo "dkim_key_length = 4096" >> /etc/postfix/dkim.conf
+echo "dkim_sign_policy = strict" >> /etc/postfix/dkim.conf
+sudo postconf -e "dkim_genkey = yes"
+sudo service postfix restart
