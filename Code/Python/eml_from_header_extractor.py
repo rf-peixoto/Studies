d86c0ca63dc48a2aa6b6a1e7e84e3d55136c9c0f
Corvo <rodrigo-f-p@hotmail.com>
2023-08-11 21:02:56 -0300

    Update eml_from_header_extractor.py

--- Code/Python/eml_from_header_extractor.py
+++ Code/Python/eml_from_header_extractor.py
@@ -242,7 +242,7 @@ def main():
 if __name__ == "__main__":
     main()
 """
-
+"""
 # Holy crow, 4th version, here we go:
 import os
 import email
@@ -256,6 +256,98 @@ def extract_emails_from_header(header_value):
         header_text = decoded_header[0].decode('utf-8', errors='replace')
     else:
         header_text = decoded_header[0]
+
+    emails = []
+    for word in header_text.split():
+        if "@" in word:
+            emails.append(word)
+    return emails
+
+def extract_domain_from_eml(eml_file_path):
+    with open(eml_file_path, 'rb') as eml_file:
+        raw_email = eml_file.read()
+
+        result = chardet.detect(raw_email)
+        encoding = result['encoding']
+
+        msg = email.message_from_bytes(raw_email)
+        
+        reply_to_header = msg.get('Reply-To')
+        from_header = msg.get('From')
+
+        emails = []
+
+        if reply_to_header:
+            emails.extend(extract_emails_from_header(reply_to_header))
+        
+        if from_header:
+            emails.extend(extract_emails_from_header(from_header))
+
+        for email_address in emails:
+            if "@" in email_address:
+                domain = email_address.split('@')[-1]
+                return domain
+
+    return None
+
+def process_eml_folder(folder_path):
+    domains_count = defaultdict(int)
+    error_log = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.eml'):
+            eml_file_path = os.path.join(folder_path, filename)
+            try:
+                sender_domain = extract_domain_from_eml(eml_file_path)
+                if sender_domain and sender_domain != 'pot':
+                    domains_count[sender_domain] += 1
+                else:
+                    error_log.append(f"No valid sender found in {filename}")
+            except Exception as e:
+                error_log.append(f"Error processing {filename}: {str(e)}")
+
+    return domains_count, error_log
+
+def main():
+    folder_path = 'path/to/your/eml/folder'
+    domains_count, error_log = process_eml_folder(folder_path)
+
+    total_domains = len(domains_count)
+    total_files = sum(domains_count.values())
+
+    print(f"Total unique domains found: {total_domains}")
+    print(f"Total EML files processed: {total_files}")
+    print("Domain distribution (sorted by file count):")
+    for domain, count in sorted(domains_count.items(), key=lambda x: x[1], reverse=True):
+        print(f"{domain}: {count} files")
+
+    if error_log:
+        with open('error.log', 'w') as error_file:
+            for error in error_log:
+                error_file.write(error + '\n')
+
+        print(f"Total errors encountered: {len(error_log)}")
+
+if __name__ == "__main__":
+    main()
+"""
+
+# Starting to panic right now
+import os
+import email
+from email.header import decode_header
+from collections import defaultdict
+import chardet
+
+def extract_emails_from_header(header_value):
+    decoded_header = decode_header(header_value)[0]
+    if isinstance(decoded_header[0], bytes):
+        header_text = decoded_header[0].decode('utf-8', errors='replace')
+    else:
+        header_text = decoded_header[0]
+
+    # Remove trailing ">" characters, if present
+    header_text = header_text.rstrip('>')
 
     emails = []
     for word in header_text.split():
