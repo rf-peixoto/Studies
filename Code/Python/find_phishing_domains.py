cffa2e41b5cc6d67fb227d6070559695c7486f25
Corvo <rodrigo-f-p@hotmail.com>
2023-10-05 14:00:52 -0300

    Update find_phishing_domains.py

--- Code/Python/find_phishing_domains.py
+++ Code/Python/find_phishing_domains.py
@@ -1,15 +1,28 @@
-# Import required modules
 import subprocess
 import json
 import sys
 
 def find_phishing_domains(target_domain):
-    # Run dnstwist and capture the output
-    result = subprocess.run(['dnstwist', '--json', target_domain], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    
-    # Parse the JSON output
-    domain_data = json.loads(output)
+    try:
+        # Run dnstwist and capture the output
+        result = subprocess.run(['dnstwist', '--json', target_domain], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Check for errors
+        if result.returncode != 0:
+            print(f"Error running dnstwist: {result.stderr.decode('utf-8')}")
+            return None
+        
+        output = result.stdout.decode('utf-8')
+        
+        # Parse the JSON output
+        domain_data = json.loads(output)
+        
+    except json.JSONDecodeError:
+        print("Error decoding JSON output from dnstwist.")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
     
     # List to store potential phishing domains
     phishing_domains = []
@@ -25,6 +38,10 @@ def find_phishing_domains(target_domain):
     return phishing_domains
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python {0} <target_domain>".format(sys.argv[0]))
+        sys.exit(1)
+    
     target_domain = sys.argv[1]
     phishing_domains = find_phishing_domains(target_domain)
     
