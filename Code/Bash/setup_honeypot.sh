c94532b474f484da640e8bb9e8f1fca360c89b4f
Corvo <rodrigo-f-p@hotmail.com>
2023-10-11 11:42:14 -0300

    Create setup_honeypot.sh

---
+++ Code/Bash/setup_honeypot.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Install Apache and PHP if not installed
+if ! dpkg -l | grep -q apache2; then
+  sudo apt update
+  sudo apt install -y apache2
+fi
+
+if ! dpkg -l | grep -q php; then
+  sudo apt install -y php libapache2-mod-php
+fi
+
+# Create directories and files
+mkdir -p /var/www/html/honeypot/uploads
+
+# Create upload.html
+echo '<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select PHP file to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload File" name="submit">
+</form>
+
+</body>
+</html>' > /var/www/html/honeypot/upload.html
+
+# Create upload.php
+echo '<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
+?>' > /var/www/html/honeypot/upload.php
+
+# Create .htaccess in uploads/ to disable PHP execution
+echo 'php_flag engine off' > /var/www/html/honeypot/uploads/.htaccess
+
+# Change ownership and permissions
+sudo chown -R www-data:www-data /var/www/html/honeypot
+sudo chmod -R 755 /var/www/html/honeypot
+
+# Enable .htaccess usage and restart Apache
+sudo a2enmod rewrite
+echo '<Directory /var/www/html/honeypot/uploads>
+  AllowOverride All
+</Directory>' | sudo tee -a /etc/apache2/apache2.conf > /dev/null
+
+sudo systemctl restart apache2
+
+echo "Honeypot setup complete. Visit http://localhost/honeypot/upload.html to upload files."
