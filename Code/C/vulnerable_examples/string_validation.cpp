8fd21ab1f828ed79241c299bc9e8596d4f79fa4a
Corvo <rodrigo-f-p@hotmail.com>
2023-10-16 11:49:16 -0300

    Update string_validation.cpp

--- Code/C/vulnerable_examples/string_validation.cpp
+++ Code/C/vulnerable_examples/string_validation.cpp
@@ -3,9 +3,13 @@ using namespace std;
 #include <iostream>
 
 int main() {
-	char input[30];
+	char input[10];
 	cout << "Input your stuff: ";
 	cin >> input;
 	printf(input);
 	return 0;
 }
+
+/*
+Exploitantion examples: %p %s %d
+*/
