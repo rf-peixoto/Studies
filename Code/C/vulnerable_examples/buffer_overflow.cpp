3638fe06bbe17ed05ddbe71e2e6f6f139069c4f0
Corvo <rodrigo-f-p@hotmail.com>
2023-10-16 11:29:13 -0300

    Create buffer_overflow.cpp

---
+++ Code/C/vulnerable_examples/buffer_overflow.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+#include <iostream>
+#include <cstring>
+
+int main() {
+	char buffer[8];
+	cout << "Random question to your input: ";
+	cin >> buffer;
+	cout << buffer <<endl;
+	return 0;
+}
