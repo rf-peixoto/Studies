22d9abd480f9ba0b01f65046ea9ee4c62ff8ae14
Corvo <rodrigo-f-p@hotmail.com>
2023-12-04 12:37:47 -0300

    Create downtime_monitor.go

---
+++ Code/Go/downtime_monitor.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	var connectionLostTime time.Time
+	var totalDowntime time.Duration
+	var lastLoggedMonth time.Month
+
+	for {
+		if isConnected() {
+			if !connectionLostTime.IsZero() {
+				connectionReturnTime := time.Now()
+				downtime := connectionReturnTime.Sub(connectionLostTime)
+				totalDowntime += downtime
+				logConnectionEvent(connectionLostTime, connectionReturnTime, downtime)
+				connectionLostTime = time.Time{}
+			}
+		} else {
+			if connectionLostTime.IsZero() {
+				connectionLostTime = time.Now()
+			}
+		}
+
+		if time.Now().Month() != lastLoggedMonth && time.Now().Format("02-15:04:05") == "30-23:59:59" {
+			logMonthlyDowntime(time.Now(), totalDowntime)
+			totalDowntime = 0
+			lastLoggedMonth = time.Now().Month()
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func isConnected() bool {
+	_, err := net.DialTimeout("tcp", "8.8.8.8:53", 1*time.Second)
+	return err == nil
+}
+
+func logConnectionEvent(lostTime, returnTime time.Time, downtime time.Duration) {
+	message := fmt.Sprintf("Connection lost at: %s\nConnection returned at: %s\nTotal downtime: %s\n",
+		lostTime.Format("2006-01-02 15:04:05"),
+		returnTime.Format("2006-01-02 15:04:05"),
+		downtime)
+	fmt.Print(message)
+	appendToFile("connection_log.txt", message)
+}
+
+func logMonthlyDowntime(currentTime time.Time, totalDowntime time.Duration) {
+	monthlyMessage := fmt.Sprintf("Month: %s\nTotal Downtime This Month: %s\n",
+		currentTime.Format("January 2006"),
+		totalDowntime)
+	fmt.Print(monthlyMessage)
+	appendToFile("connection_log.txt", monthlyMessage)
+}
+
+func appendToFile(filename, text string) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(text); err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
+}
