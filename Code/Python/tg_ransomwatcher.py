c2aa73fa5211b699294c76b0b7e47579421a9011
Corvo <rodrigo-f-p@hotmail.com>
2023-12-10 19:59:55 -0300

    Update tg_ransomwatcher.py

--- Code/Python/tg_ransomwatcher.py
+++ Code/Python/tg_ransomwatcher.py
@@ -1,20 +1,33 @@
 import requests
 import json
 import telegram
+import logging
 from time import sleep
 import os
 
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class DataFetcher:
-    def __init__(self, url, local_file):
+    def __init__(self, url, local_file, chat_id, bot_token):
         self.url = url
         self.local_file = local_file
         self.indexed_items = set()
+        self.chat_id = chat_id
+        self.bot_token = bot_token
+        self.bot = telegram.Bot(bot_token)
 
     def download_initial_data(self):
-        response = requests.get(self.url)
-        if response.status_code == 200:
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            initial_data = response.json()
+            self.indexed_items.update(item['post_title'] for item in initial_data)
             with open(self.local_file, 'w') as file:
-                json.dump(response.json(), file)
+                json.dump(initial_data, file)
+        except requests.RequestException as e:
+            logging.error(f"Error downloading initial data: {e}")
+            self.send_error_alert(f"Error downloading initial data: {e}")
 
     def fetch_data(self):
         try:
@@ -22,8 +35,9 @@ class DataFetcher:
             response.raise_for_status()
             return response.json()
         except requests.RequestException as e:
-            print(f"Error fetching data: {e}")
-            return []
+            logging.error(f"Error fetching data: {e}")
+            self.send_error_alert(f"Error fetching data: {e}")
+            return None
 
     def read_local_data(self):
         if os.path.exists(self.local_file):
@@ -46,15 +60,25 @@ class DataFetcher:
 
     def get_new_items(self):
         web_data = self.fetch_data()
+        if web_data is None:
+            return []
+
         local_data = self.read_local_data()
         new_items = self.index_data(web_data)
         if new_items:
-            self.update_local_data(web_data)
+            self.update_local_data(local_data + new_items)
         return new_items
 
+    def send_error_alert(self, message):
+        self.bot.sendMessage(self.chat_id, f"❌ Error Alert: {message}")
+
 # Define monitor:
-monitor = DataFetcher("https://raw.githubusercontent.com/joshhighet/ransomwatch/main/posts.json", "local_data.json")
-bot = telegram.Bot("TOKEN")
+monitor = DataFetcher(
+    url="https://raw.githubusercontent.com/joshhighet/ransomwatch/main/posts.json",
+    local_file="local_data.json",
+    chat_id="YOUR_CHAT_ID",
+    bot_token="YOUR_TOKEN"
+)
 
 # Run:
 if __name__ == '__main__':
@@ -70,10 +94,11 @@ if __name__ == '__main__':
             # Fetch new items:
             for i in monitor.get_new_items():
                 msg = '❗️ Alerta de Ransomware ❗️\nPost: "{0}"\nGrupo: {1}\nIdentificado em: {2}'.format(i['post_title'], i['group_name'], i['discovered'].split(" ")[0])
-                bot.sendMessage("CHANNEL", msg)
+                monitor.bot.sendMessage(monitor.chat_id, msg)
                 sleep(30)
-            print("{0} cases indexed and reported.".format(len(monitor.indexed_items)))
+            logging.info("{0} cases indexed and reported.".format(len(monitor.indexed_items)))
             sleep(1800)
         except Exception as error:
-            print(error)
+            logging.error(f"An error occurred: {error}")
+            monitor.send_error_alert(f"An error occurred: {error}")
             sleep(15)
