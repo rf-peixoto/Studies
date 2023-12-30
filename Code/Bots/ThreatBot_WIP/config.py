128b1f6ad2185c4faf7b332ff62cb21762a29d16
Corvo <rodrigo-f-p@hotmail.com>
2023-12-30 11:34:57 -0300

    Update config.py

--- Code/Bots/ThreatBot_WIP/config.py
+++ Code/Bots/ThreatBot_WIP/config.py
@@ -1,1 +1,9 @@
+import json
 
+class Config:
+    def __init__(self, filename):
+        with open(filename, 'r') as file:
+            self.config = json.load(file)
+        
+    def __getattr__(self, name):
+        return self.config.get(name, None)
