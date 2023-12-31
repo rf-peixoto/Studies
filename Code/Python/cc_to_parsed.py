214ef3da877510e0893a8bc9158aef642c91ee9f
Corvo <rodrigo-f-p@hotmail.com>
2023-12-31 16:13:38 -0300

    Update cc_to_parsed.py

--- Code/Python/cc_to_parsed.py
+++ Code/Python/cc_to_parsed.py
@@ -10,10 +10,10 @@ def read_last_id(file_path):
         with open(file_path, 'r') as file:
             return int(file.read().strip())
     except FileNotFoundError:
-        return 0  # Start from 0 since we add 1 when parsing starts
+        return 1
     except ValueError:
         logging.warning("Last ID file is corrupted. Starting from ID 1.")
-        return 0
+        return 1
 
 def save_last_id(file_path, last_id):
     try:
@@ -22,21 +22,25 @@ def save_last_id(file_path, last_id):
     except IOError as e:
         logging.error(f"Failed to save last ID: {e}")
 
-def parse_file_to_json(file_path, output_file_path, last_id_file):
-    last_id = read_last_id(last_id_file)
+def append_rejected_line(file_path, line):
+    with open(file_path, 'a') as file:
+        file.write(f"{line}\n")
 
+def parse_file_to_json(file_path, output_file_path, last_id_file, rejected_file):
+    last_id = read_last_id(last_id_file)
     json_data = []
+
     try:
         with open(file_path, 'r') as file:
-            for line in file:
+            for file_line_number, line in enumerate(file, start=1):
                 line = line.strip()
                 if len(line) != 28:
-                    logging.warning(f"Skipping invalid line {last_id+1}: Incorrect length")
+                    logging.warning(f"Skipping invalid line {file_line_number}: Incorrect length")
+                    append_rejected_line(rejected_file, line)
                     continue
 
                 parts = line.split('|')
                 if len(parts) == 4:
-                    last_id += 1
                     json_line = {
                         "id": last_id,
                         "bin": parts[0][:6],
@@ -45,8 +49,10 @@ def parse_file_to_json(file_path, output_file_path, last_id_file):
                         "cvv": parts[3]
                     }
                     json_data.append(json_line)
+                    last_id += 1
                 else:
-                    logging.warning(f"Skipping invalid line {last_id+1}: Incorrect format")
+                    logging.warning(f"Skipping invalid line {file_line_number}: Incorrect format")
+                    append_rejected_line(rejected_file, line)
 
         with open(output_file_path, 'w') as outfile:
             json.dump(json_data, outfile, indent=4)
@@ -66,10 +72,10 @@ if __name__ == "__main__":
     file_path = sys.argv[1]
     output_file_path = "parsed_at_{0}.json".format(datetime.timestamp(datetime.now()))
     last_id_file = "last_id.txt"
+    rejected_file = "rejected_lines.txt"
     
     try:
-        parse_file_to_json(file_path, output_file_path, last_id_file)
+        parse_file_to_json(file_path, output_file_path, last_id_file, rejected_file)
         logging.info(f"Data parsed and saved to {output_file_path}")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-
