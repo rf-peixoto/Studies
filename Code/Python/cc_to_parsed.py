d83b4a7f2f542f1cef68ba52bd5965c59a7c77fe
Corvo <rodrigo-f-p@hotmail.com>
2023-12-31 13:51:32 -0300

    Update cc_to_parsed.py

--- Code/Python/cc_to_parsed.py
+++ Code/Python/cc_to_parsed.py
@@ -1,28 +1,74 @@
-import json, sys
+import json
+import sys
+import logging
 
 # Valid format: 0000000000000000|DD|YYYY|000
 
-def parse_file_to_json(file_path, output_file_path):
+def read_last_id(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return int(file.read().strip())
+    except FileNotFoundError:
+        return 0  # Start from 0 since we add 1 when parsing starts
+    except ValueError:
+        logging.warning("Last ID file is corrupted. Starting from ID 1.")
+        return 0
+
+def save_last_id(file_path, last_id):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(str(last_id))
+    except IOError as e:
+        logging.error(f"Failed to save last ID: {e}")
+
+def parse_file_to_json(file_path, output_file_path, last_id_file):
+    last_id = read_last_id(last_id_file)
 
     json_data = []
-    with open(file_path, 'r') as file:
-        for line_number, line in enumerate(file, start=1):
-            parts = line.strip().split('|')
-            if len(parts) == 4:
-                json_line = {
-                    "id": line_number,
-                    "bin": parts[0][:6],
-                    "number": parts[0],
-                    "expires": parts[1] + parts[2][-2:],
-                    "cvv": parts[3]
-                }
-                json_data.append(json_line)
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if len(line) != 28:
+                    logging.warning(f"Skipping invalid line {last_id+1}: Incorrect length")
+                    continue
 
-    with open(output_file_path, 'w') as outfile:
-        json.dump(json_data, outfile, indent=4)
+                parts = line.split('|')
+                if len(parts) == 4:
+                    last_id += 1
+                    json_line = {
+                        "id": last_id,
+                        "bin": parts[0][:6],
+                        "number": parts[0],
+                        "expires": parts[1] + parts[2][-2:],
+                        "cvv": parts[3]
+                    }
+                    json_data.append(json_line)
+                else:
+                    logging.warning(f"Skipping invalid line {last_id+1}: Incorrect format")
 
-# Example usage
-file_path = sys.argv[1]
-output_file_path = "output.json"
-parse_file_to_json(file_path, output_file_path)
-print(f"Data parsed and saved to {output_file_path}")
+        with open(output_file_path, 'w') as outfile:
+            json.dump(json_data, outfile, indent=4)
+
+        save_last_id(last_id_file, last_id)
+
+    except IOError as e:
+        logging.error(f"File operation failed: {e}")
+        raise
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    if len(sys.argv) < 2:
+        logging.error("Usage: python script.py <input_file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    output_file_path = "output.json"
+    last_id_file = "last_id.txt"
+    
+    try:
+        parse_file_to_json(file_path, output_file_path, last_id_file)
+        logging.info(f"Data parsed and saved to {output_file_path}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
