261f6c536b91600266a9e4351e6981621b4f14e0
Corvo <rodrigo-f-p@hotmail.com>
2024-02-05 00:28:32 -0300

    Update stealer_log_parser.py

--- Code/Python/stealer_log_parser.py
+++ Code/Python/stealer_log_parser.py
@@ -1,11 +1,17 @@
 import os
 import re
+from datetime import datetime
 
 def find_and_parse_passwords(root_dir, output_file):
     # Regex to match the structure in the Passwords.txt file
     entry_pattern = re.compile(r'^URL: (https?://[^\s]+)\s+Username: ([^\s]+)\s+Password: ([^\s]+)\s+Application: (.+)$', re.MULTILINE)
     
-    with open(output_file, 'w', encoding='utf-8') as outfile:
+    # Generate output file path with timestamp
+    current_time = datetime.now()
+    timestamp = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    output_file_with_timestamp = f'{output_file}_{timestamp}.txt'
+    
+    with open(output_file_with_timestamp, 'w', encoding='utf-8') as outfile:
         for root, dirs, files in os.walk(root_dir):
             if 'Passwords.txt' in files:
                 try:
@@ -21,6 +27,6 @@ def find_and_parse_passwords(root_dir, output_file):
                     print(f"Encoding error in file {os.path.join(root, 'Passwords.txt')}: {e}")
 
 # Example usage
-root_directory = r'C:\Path\to\root\dir'  # Update this to your starting directory
-output_file_path = 'parsed_passwords.txt'  # Output file where the results will be saved
-find_and_parse_passwords(root_directory, output_file_path)
+root_directory = r'C:\path\to\your\folder'  # Update this to your starting directory
+output_file_base = 'parsed_passwords'  # Base name for the output file
+find_and_parse_passwords(root_directory, output_file_base)
