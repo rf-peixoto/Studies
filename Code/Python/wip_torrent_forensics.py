8baaa9c0b05407901dab31a6dea0b86b14836054
Corvo <rodrigo-f-p@hotmail.com>
2024-06-03 10:00:53 -0300

    Create wip_torrent_forensics.py

---
+++ Code/Python/wip_torrent_forensics.py
@@ -0,0 +1,38 @@
+#pip install python-libtorrent
+
+import libtorrent as lt
+import time
+
+def track_torrent(magnet_link):
+    ses = lt.session()
+    params = {
+        'save_path': './',
+        'storage_mode': lt.storage_mode_t(2),
+        'paused': False,
+        'auto_managed': True,
+        'duplicate_is_error': True
+    }
+    handle = lt.add_magnet_uri(ses, magnet_link, params)
+
+    print('Downloading metadata...')
+    while not handle.has_metadata():
+        time.sleep(1)
+    print('Metadata downloaded.')
+
+    torrent_info = handle.get_torrent_info()
+    print('Torrent name:', torrent_info.name())
+
+    print('\nTracking peers and seeds...\n')
+    while True:
+        status = handle.status()
+        print('Peers:', status.num_peers)
+        print('Seeds:', status.num_seeds)
+        print('Download rate:', status.download_rate / 1000, 'kB/s')
+        print('Upload rate:', status.upload_rate / 1000, 'kB/s')
+        print('Progress:', status.progress * 100, '%')
+        print('-' * 40)
+        time.sleep(5)
+
+if __name__ == '__main__':
+    magnet_link = input('Enter the magnet link: ')
+    track_torrent(magnet_link)
