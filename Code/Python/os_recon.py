4ecd9523d72ce4e358e0541b89bc60c3ae68ef45
Corvo <rodrigo-f-p@hotmail.com>
2025-01-31 11:41:30 -0300

    Update os_recon.py

--- Code/Python/os_recon.py
+++ Code/Python/os_recon.py
@@ -4,12 +4,13 @@ from datetime import datetime
 
 class OSRecon:
     def __init__(self):
-        self.os = platform.system()
+        self.os = platform.system() + " " + platform.version()
         self.arch = "{0} {1}".format(platform.machine(), platform.architecture()[0])
         self.username = platform.os.getlogin()
         self.home = str(Path.home())
         self.node = platform.node()
         self.node_ip = socket.gethostbyname(socket.gethostname())
+        self.processor = platform.processor()
         try:
             self.dump = platform.freedesktop_os_release()
         except:
@@ -24,4 +25,4 @@ print("Home Path: {0}".format(r.home))
 print("Network Node: {0}".format(r.node))
 print("Local Address: {0}".format(r.node_ip))
 print("\n")
-print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}|{r.username}|{r.os} {r.arch}|{r.home}|{r.node}|{r.node_ip}")
+print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}|{r.username}|{r.os} {r.arch}|{r.processor}|{r.home}|{r.node}|{r.node_ip}")
