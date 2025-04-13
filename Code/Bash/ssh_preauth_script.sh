18ea8b1b9a5e21f2c38769d44f0346d9f1900e86
Corvo <rodrigo-f-p@hotmail.com>
2025-04-13 16:25:59 -0300

    Create ssh_preauth_script.sh

---
+++ Code/Bash/ssh_preauth_script.sh
@@ -0,0 +1,7 @@
+/etc/ssh/sshrc
+ Commands in this file are executed by ssh when the user
+ logs in, just before the user's shell (or command) is
+ started. See the sshd(8) manual page for more
+ information.
+
+ref: https://www.linkedin.com/posts/luciano-marcos_se-tem-algo-que-eu-gosto-%C3%A9-seguran%C3%A7a-da-informa%C3%A7%C3%A3o-activity-7317211747042353154-zH-3
