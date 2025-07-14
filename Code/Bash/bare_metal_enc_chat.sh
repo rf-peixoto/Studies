ca74ed3bc6c8ea19c1d58b4b8c2503113ea1f95c
Corvo <rodrigo-f-p@hotmail.com>
2025-07-14 10:20:58 -0300

    Create bare_metal_enc_chat.sh

---
+++ Code/Bash/bare_metal_enc_chat.sh
@@ -0,0 +1,1 @@
+while true; do read -n30 ui; echo $ui |openssl enc -aes-256-ctr -a -k PaSSw; done | nc -l -p 8877 | while read so; do decoded_so=echo "$so"| openssl enc -d -a -aes-256-ctr -k PaSSw; echo -e "Incoming: $decoded_so"; done
