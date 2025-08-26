99b0ac83d428af3e0bfd7f18ee76522bd947357b
Corvo <rodrigo-f-p@hotmail.com>
2025-08-26 10:00:39 -0300

    Update android_ssl_pinnin.sh

--- Code/Bash/android_ssl_pinnin.sh
+++ Code/Bash/android_ssl_pinnin.sh
@@ -12,3 +12,11 @@ openssl pkey -punin -in publick_key.pem -outform DER -out pub.der
 openssl dgst -sha256 -binary pub.der | base64
 
 # Dev would pin this hash on the code and check the certificate every connection.
+
+
+# If you need the .pem based on a .der:
+openssl x509 -inform DER cert.der -out cert.pem
+
+# Android will use <hash>.0 format, so:
+openssl x509 -inform PEM -subject_hash_old -in cert.pem | head -1
+# them rename the cert file to the <hash>.0 you got. Ex: mv cert.pem 1234abcd.0
