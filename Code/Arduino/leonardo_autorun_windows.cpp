0b675531fd7872f4be8181a30f478ec3ba98904e
Corvo <rodrigo-f-p@hotmail.com>
2026-05-22 12:21:45 -0300

    Update and rename leonardo_autorun_windows.txt to leonardo_autorun_windows.cpp

--- Code/Arduino/leonardo_autorun_windows.cpp
+++ Code/Arduino/leonardo_autorun_windows.cpp
@@ -3,22 +3,26 @@
 void setup() {
     // Start keyboard emulation
     Keyboard.begin();
+
     // Give Windows time to recognize the device
     delay(5000);
+
     // Open Run dialog (Win + R)
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press('r');
     delay(100);
     Keyboard.releaseAll();
+
     delay(1000);
 
     // Open cmd
     Keyboard.print("cmd");
     Keyboard.write(KEY_RETURN);
+
     delay(1000);
 
     // Your backup command here
-    Keyboard.print("C:\\scripts\\backup.bat"); // Edit the command here
+    Keyboard.print("echo Arduino test > %USERPROFILE%\\Downloads\\arduino_test.txt");
     Keyboard.write(KEY_RETURN);
 
     // Stop keyboard emulation
